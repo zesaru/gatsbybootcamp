2ca709090aafd56a96bc3014c3c6db739c13a9ac
Cesar Murillo <namiayumis@gmail.com>
2019-09-17 16:42:13 +0900

    create contact blog about pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>
+        I'm Andrew, a full-stack developers living in beatifull Philadelphia
+      </h2>
+    </div>
+  )
+}
+
+export default IndexPage
