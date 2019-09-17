b47d536e87ed5871c65c56c1cc6494a1870cc666
Cesar Murillo <namiayumis@gmail.com>
2019-09-17 21:11:33 +0900

    Linking Between Pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const ContactPage = () => {
   return (
     <div>
       <h1>Contact Page</h1>
+      <p>
+        Contact me
+        <a href="https://twitter.com/@zesarum" target="_blank">
+          @zesarum
+        </a>
+      </p>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { List, Link } from "gatsby"
 
 const IndexPage = () => {
   return (
@@ -7,6 +8,9 @@ const IndexPage = () => {
       <h2>
         I'm Andrew, a full-stack developers living in beatifull Philadelphia
       </h2>
+      <p>
+        Need a developers ? <Link to="/contact">Contact me</Link>
+      </p>
     </div>
   )
 }
