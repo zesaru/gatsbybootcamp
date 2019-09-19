2090980d14bc4610a9d802d88a0123c52661f45f
Cesar Murillo <namiayumis@gmail.com>
2019-09-19 14:23:22 +0900

    Working with Markdown Posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
     title: "Full-Stack Bootcamp",
     author: "Cesar Murillo",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }
