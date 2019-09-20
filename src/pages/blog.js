0abb1d8a1af8241d43d09ce9bc95ef8a9bacca3b
Cesar Murillo <namiayumis@gmail.com>
2019-09-20 18:25:20 +0900

    Adding Images to Posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import { Link, graphql, useStaticQuery } from "gatsby"
+import blogStyles from "./blog.module.scss"
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -25,16 +26,16 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ol>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map(edge => {
           return (
-            <li>
+            <li className={blogStyles.post}>
               <h2>
                 <Link to={`/blog/${edge.node.fields.slug}`}>
-                  {edge.node.frontmatter.title}
+                  <h2>{edge.node.frontmatter.title}</h2>
+                  <p>{edge.node.frontmatter.date}</p>
                 </Link>
               </h2>
-              <p>{edge.node.frontmatter.date}</p>
             </li>
           )
         })}
